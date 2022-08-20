94177b3b431fb265b5ddf937aa9efafe558ea8b2
Yasiru  Amarasinghe <aaayasiru@gmail.com>
2022-08-20 09:17:20 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,89 @@
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//Api Key\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//aumto functioner
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true 
+//documents variants
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//owmner v card
+global.vcardowner = ['94719876337'] //ur owner number
+global.ownername = "Nuwa"//ur owner name
+global.socialm = " Nuwa" 
+global.location = "SriLanka, Western province" 
+global.owner = ['94719876337']
+global.ownertag = '94719876337' //ur tag number
+global.botname = 'Nuwa 360°' //ur bot name
+global.linkz = "http://wa.me/94719876337" //your theme url which will be displayed on whatsapp
+global.websitex = "http://wa.me/94719876337" //ur website to be displayed
+global.reactmoji = "🤽" //ur menu react emoji
+global.themeemoji = "🙆" //ur theme emoji
+global.packname = "\n\n\n\n nuwa " //ur sticker watermark packname
+global.author = "Nuwa Inc | 2022" //ur sticker watermark author
+
+//Bot theme media
+global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
+global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") 
+global.premium = ['916909137213'] 
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '🌝'
+global.mess = {
+    success: 'Done ✓',
+    admin: 'This Feature Is Only For Admin!',
+    botAdmin: 'Bot Must Be Admin First!',
+    owner: 'This Feature Is Only For Owner!',
+    group: 'Feature Used Only For Groups!',
+    private: 'Features Used Only For Private Chat!',
+    bot: 'This Feature Is Only For Bot',
+    wait: 'In process...',
+    linkm: 'Where is the link?',
+    error: 'Error!!',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+global.ehi{
+	url1:'https://www.mediafire.com/file/nli6bvkgiuthe60/%25F0%259F%259A%2580_%25E1%25B4%25A1%25CA%259C%25E1%25B4%2580%25E1%25B4%259B%25EA%259C%25B1%25E1%25B4%2580%25E1%25B4%2598%25E1%25B4%2598_%25CA%2599%25CA%258F%25E1%25B4%2598%25E1%25B4%2580%25EA%259C%25B1%25EA%259C%25B1%25F0%259F%259A%2580.ehi/file',
+	url2:'https://www.mediafire.com/file/affoujdnz1ndmee/%25F0%259F%259A%2580%25E1%25B4%2598%25E1%25B4%2580%25C9%25AA%25E1%25B4%2585_%25E1%25B4%259B%25C9%25AA%25E1%25B4%258B%25E1%25B4%259B%25E1%25B4%258F%25E1%25B4%258B%25F0%259F%259A%2580.ehi/file',
+	url3:'https://www.mediafire.com/file/c5uzzwsou2gcg2m/%25F0%259F%259A%2580%25E1%25B4%2598%25E1%25B4%2580%25C9%25AA%25E1%25B4%2585_%25E1%25B4%25A1%25CA%259C%25E1%25B4%2580%25E1%25B4%259B%25EA%259C%25B1%25E1%25B4%2580%25E1%25B4%2598%25E1%25B4%2598%25F0%259F%259A%2580.ehi/file',
+	url4:'https://www.mediafire.com/file/ltly0gf9fc56lsu/%25F0%259F%259A%2580%25E1%25B4%2598%25E1%25B4%2580%25C9%25AA%25E1%25B4%2585_%25E1%25B4%25A2%25E1%25B4%258F%25E1%25B4%258F%25E1%25B4%258D%25F0%259F%259A%2580.ehi/file',
+	url5:'https://www.mediafire.com/file/e6x0keykpy6hihb/%25F0%259F%259A%2580%25E1%25B4%2598%25E1%25B4%2580%25C9%25AA%25E1%25B4%2585_%25CA%258F%25E1%25B4%258F%25E1%25B4%259C%25E1%25B4%259B%25E1%25B4%259C%25CA%2599%25E1%25B4%2587%25F0%259F%259A%2580.ehi/file',
+	url6:'https://www.mediafire.com/file/ra2zzoe8ztemvpa/%25F0%259F%259A%2580%25E1%25B4%2598%25E1%25B4%2580%25C9%25AA%25E1%25B4%2585_%25EA%259C%25B0%25E1%25B4%2580%25E1%25B4%2584%25E1%25B4%2587%25CA%2599%25E1%25B4%258F%25E1%25B4%258F%25E1%25B4%258B%25F0%259F%259A%2580.ehi/file'
+}
+global.thumb = fs.readFileSync('./XeonMedia/theme/cheemspic.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
